6ab59eaf82d5276d0bae7608dfca60e5aa296c34
bplatt <rplatt@cscc.edu>
2021-03-02 14:32:48 -0500

    Initial commit

---
+++ src/edu/cscc/Main.java
@@ -0,0 +1,12 @@
+package edu.cscc;
+
+/**
+ * Hello World example
+ * @author bplatt
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
